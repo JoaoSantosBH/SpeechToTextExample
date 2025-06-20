ffb8ae9cd40cc3f751c3d06e27a83f836ee5ecc5
João Santos <joaomarcelo.ms@gmail.com>
2025-06-19 21:04:25 -0300

    removendo libs desnecessarias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,16 +58,12 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.androidx.material2.icons)
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.compose.ui:ui:1.5.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
-    implementation("androidx.compose.material3:material3:1.1.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("com.google.accompanist:accompanist-permissions:0.32.0")
 
 
